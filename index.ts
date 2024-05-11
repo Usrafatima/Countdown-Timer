d4021776035b22cc516bb2b78d32d3ff1021b063
Yusra <yusra7003@gmail.com>
2024-05-11 22:56:40 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import { differenceInSeconds } from "date-fns";
+
+function* countdownTimer(seconds: number) {
+    while (seconds > 0) {
+        yield seconds;
+        seconds--;
+    }
+}
+
+let timer = countdownTimer(10);
+
+function displaycountdown() {
+    let result = timer.next();
+
+    if (!result.done) {
+        const now = new Date();
+        const countdownTime = new Date(now.getTime() + (result.value * 1000));
+        const remainingSeconds = differenceInSeconds(countdownTime, now);
+        console.log(`Remaining Seconds : ${remainingSeconds}`);
+        setTimeout(displaycountdown, 1000); 
+        } else {
+        console.log("Countdown Complete");
+    }
+}
+displaycountdown();
